f88154f17c0671293a50ccf5719c4dc001b3d6e8
srapltd <srappltd@gmail.com>
2023-08-04 16:41:05 +0530

    wave edit

--- script/script.js
+++ script/script.js
@@ -504,3 +504,14 @@ function page13(){
     })
 }
 
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
